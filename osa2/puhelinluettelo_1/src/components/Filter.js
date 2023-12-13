eb1c448aa6c67920f2d8d0c1ed49caaa431f81c0
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2023-12-13 16:04:21 +0200

    Delete osa2 directory

--- osa2/puhelinluettelo_1/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = ({searchText, setSearchText}) => {
-    return (
-        <div>
-          search: <input type="text" value={searchText} onChange={({target}) => setSearchText(target.value)}/>
-        </div>
-    )
-}
-
-export default Filter
