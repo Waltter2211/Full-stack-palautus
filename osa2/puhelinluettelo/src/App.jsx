d522e5e70e911ca163b4be506911e9aa3a426180
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-03-18 15:50:03 +0200

    puhelinluettelo step4

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
   const handleNameInput = event => setNewName(event.target.value)
   const handleNumberInput = event => setNewNumber(event.target.value)
+  const handleSearchInput = event => {
+    const regex = new RegExp(event.target.value, 'i')
+    const filteredNames = persons.filter(person => person.name.match(regex))
+    setPersons(filteredNames)
+  }
 
   const onSubmit = event => {
     event.preventDefault()
@@ -24,6 +32,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with: <input onChange={handleSearchInput} /></div>
       <form>
         <div>
           name: <input onChange={handleNameInput} /> <br />
