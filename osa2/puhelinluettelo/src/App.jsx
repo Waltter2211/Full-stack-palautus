04788762710028bf84c589391a6ee2918af0ee5d
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-03-18 14:56:21 +0200

    puhelinluettelo step1

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleInput = event => setNewName(event.target.value)
+
+  const onSubmit = event => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input onChange={handleInput} />
+        </div>
+        <div>
+          <button type="submit" onClick={onSubmit}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, i) => <p key={i}>{person.name}</p>)}
+    </div>
+  )
+
+}
+
+export default App
