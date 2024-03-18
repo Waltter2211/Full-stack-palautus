f104564bb6444d96f22f9de5cbdb05e42f49bc3c
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-03-18 14:03:32 +0200

    kurssitiedot step7&8

--- osa1/kurssitiedot/src/components/Content.jsx
+++ osa1/kurssitiedot/src/components/Content.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 import Part from './Part'
 
 function Content({ parts }) {
+
+    const exercisesArr = parts.map((part) => {
+        return part.exercises
+    })
+
+    const total = exercisesArr.reduce((acc, curr) => {
+        return acc + curr
+    })
+
   return (
     <div>
         {parts.map((part) => {
             return <Part key={part.id} part={part} />
         })}
+        <h4>total of {total} exercises</h4>
     </div>
   )
 }
--- osa1/kurssitiedot/src/components/Part.jsx
+++ osa1/kurssitiedot/src/components/Part.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 function Part({ part }) {
-    console.log("propsit",part)
   return (
     <p>{part.name} {part.exercises}</p>
   )
