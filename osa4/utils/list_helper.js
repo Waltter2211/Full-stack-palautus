94d67e9185fc81113fe731b659b1d2fc15678ea3
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-03-22 14:41:24 +0200

    apufunktioita ja yksikkötestejä step2

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+  const totalLikes = blogs.map((blog) => blog.likes).reduce((acc, curr) => acc + curr)
+  return (
+    blogs.length === 1
+    ? blogs[0].likes 
+    : totalLikes
+  )
+  
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
