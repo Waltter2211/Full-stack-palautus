0a0c9d4f48c1798b747e887f8d5634ef7571336c
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-03-23 20:19:48 +0200

    blogilistan laajennus step5

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,15 +1,35 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
   
 blogsRouter.post('/', async (request, response) => {
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
   const blog = new Blog(request.body)
+  blog.user = user._id
   
+  user.blogs = user.blogs.concat(blog._id)
+  await user.save()
   await blog.save()
+  
   response.status(201).json(blog)
 })
 
