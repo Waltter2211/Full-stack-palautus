cee75adb1b4252eb23ba59267c54c9332bc2639c
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-03-23 18:46:47 +0200

    blogilistan laajennus step2&3

---
+++ osa4/controllers/users.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).populate('blogs', { title: 1, author: 1, likes: 1, url: 1 })
+  response.json(users)
+})
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  if (password.length < 3) {
+    response.status(403).send({ error: "password must be atleast 3 characters long" })
+  }
+  else {
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+  
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    })
+
+    const savedUser = await user.save()
+
+    response.status(201).json(savedUser)
+  }
+
+  
+
+  
+})
+
+module.exports = usersRouter
