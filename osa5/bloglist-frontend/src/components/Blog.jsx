8a437b7319bc6b1c6ec691430b603c2425f5a545
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-03-26 18:14:15 +0200

    blogilistan testit step2

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -26,13 +26,13 @@ const Blog = ({ blog, user }) => {
     <div>
       {visible? <div style={style}>
         <p className='blog'>{blog.title}<button onClick={() => setVisible(!visible)}>hide</button></p>
-        <p>{blog.url}</p>
-        <p>likes {blog.likes}<button onClick={() => {
+        <p data-testid="url">{blog.url}</p>
+        <p data-testid="likes">likes {blog.likes}<button onClick={() => {
           handleBlogUpdate()
         /* setLike(like + 1) */
         }}>like</button></p>
         <p>{blog.author}</p>
-        {user.name === blog.user.name && <button onClick={handleBlogDelete}>remove</button>}
+        {/* {user.name === blog.user.name && <button onClick={handleBlogDelete}>remove</button>} */}
       </div>:<div style={style}>{blog.title}<button onClick={() => setVisible(!visible)}>view</button></div>}
     </div>
   )
--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,17 +1,34 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
+import { expect } from 'vitest'
 
-test('Blog is rendered successfully', async () => {
+describe('<Blog />', () => {
     const blog = {
         title: 'Component testing is done with react-testing-library',
         author: 'testing author',
         url: 'testing url',
         likes: 23
-      }
+    }
+  
+    beforeEach(() => {
+      render(<Blog blog={blog} />)
+    })
 
-  render(<Blog blog={blog} />)
+    test('Blog is rendered successfully', async () => {
+        const element = screen.getByText('Component testing is done with react-testing-library')
+        expect(element).toBeDefined()
+        screen.debug(element)
+    })
 
-  const element = screen.getByText('Component testing is done with react-testing-library')
-  expect(element).toBeDefined()
-  screen.debug(element)
+    test('Blog can be expanded to view details', async () => {
+        const user = userEvent.setup()
+
+        const button = screen.getByText('view')
+        await user.click(button)
+        const url = screen.getAllByTestId('url')
+        const likes = screen.getAllByTestId('likes')
+        expect(url, likes).toBeDefined()
+    })
+
 })
