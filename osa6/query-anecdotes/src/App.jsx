831a50f6e57b23d43cc7100b6a1e1f12ac637a9c
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-03-29 23:43:58 +0200

    anecdotes react query step2

--- osa6/query-anecdotes/src/App.jsx
+++ osa6/query-anecdotes/src/App.jsx
@@ -1,11 +1,9 @@
-import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { getAnecdotes } from './requests'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
-
-  const queryClient = useQueryClient()
 
   const handleVote = (anecdote) => {
     console.log('vote')
