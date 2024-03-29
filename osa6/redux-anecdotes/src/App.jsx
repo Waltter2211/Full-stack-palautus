f13e1485d883334984f5872f96ba918b002c9603
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-03-29 04:09:00 +0200

    paremmat anekdootit step7

--- osa6/redux-anecdotes/src/App.jsx
+++ osa6/redux-anecdotes/src/App.jsx
@@ -1,11 +1,16 @@
+import { useSelector } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm.jsx'
 import AnecdoteList from './components/AnecdoteList.jsx'
+import Filter from './components/Filter.jsx'
 
 const App = () => {
+
+
   
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Filter />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,8 +2,16 @@ import { useSelector, useDispatch } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
 
 function AnecdoteList() {
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => {
+        if (state.filter === 'ALL') {
+            return state.anecdotes
+        }
+        else {
+            return state.anecdotes.filter((anecdote) => anecdote.content.includes(state.filter))
+        }
+    })
     const dispatch = useDispatch()
+
   return (
     <div>
         {anecdotes.map(anecdote =>
