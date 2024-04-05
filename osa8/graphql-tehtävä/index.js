4c9528063ec6f37020177da936636e43636af846
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-04-05 05:57:54 +0300

    kirjailijan kirjat graphql

--- osa8/graphql-tehtävä/index.js
+++ osa8/graphql-tehtävä/index.js
@@ -116,7 +116,7 @@ const typeDefs = `
     type Query {
         bookCount: Int!
         authorCount: Int!
-        allBooks: [Book!]!
+        allBooks(author: String): [Book!]!
         allAuthors: [Author!]!
     }
 `
@@ -125,7 +125,14 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+        if (!args.author) {
+            return books
+        }
+        else {
+            return books.filter(book => book.author === args.author)
+        }
+    },
     allAuthors: () => authors.map(({name}) => {
         const bookCount = books.filter((b) => b.author === name).length
         return {
