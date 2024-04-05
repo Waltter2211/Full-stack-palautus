c0a576a35a7095a66bd9cb24633700b68cb65f0f
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-04-05 06:21:10 +0300

    genren kirjat graphql

--- osa8/graphql-tehtävä/index.js
+++ osa8/graphql-tehtävä/index.js
@@ -116,7 +116,7 @@ const typeDefs = `
     type Query {
         bookCount: Int!
         authorCount: Int!
-        allBooks(author: String): [Book!]!
+        allBooks(author: String, genres: String): [Book!]!
         allAuthors: [Author!]!
     }
 `
@@ -126,11 +126,14 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-        if (!args.author) {
-            return books
+        if (args.author) {
+            return books.filter(book => book.author === args.author)
+        }
+        else if (args.genres) {
+            return books.filter(b => b.genres.includes(args.genres))
         }
         else {
-            return books.filter(book => book.author === args.author)
+            return books
         }
     },
     allAuthors: () => authors.map(({name}) => {
