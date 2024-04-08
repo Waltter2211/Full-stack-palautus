a5019cccc69890cc9ad387004cd7c9f6344407af
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-04-09 02:55:01 +0300

    Body mass index ts

---
+++ osa9/BMI-Calculator/bmi-calculator.ts
@@ -0,0 +1,17 @@
+const weight: number = 0;
+const height: number = 0;
+
+const calculateBmi = (height: number, weight: number): string => {
+    const bmi: number = weight / (height * height)*10000;
+    if (bmi < 18.5) {
+        return 'Underweight'
+    }
+    else if (bmi > 18.5 && bmi < 25) {
+        return 'Normal (healthy weight)'
+    }
+    else if (bmi > 25) {
+        return 'Overweight'
+    }
+}
+
+console.log(calculateBmi(180, 74))
