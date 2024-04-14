68a46d28ed6e3ff7b79087a058eeb99b8d487481
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-04-14 23:51:55 +0300

    Patientor, step 8 ts

--- osa9/Patientor-harjoitus/patientor/src/components/PatientInfoPage/index.tsx
+++ osa9/Patientor-harjoitus/patientor/src/components/PatientInfoPage/index.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import patientService from '../../services/patients';
-import { Diagnose, Entry, Patient } from '../../types';
-import getDiagnoses from '../../services/diagnoses';
+import { Diagnose, Entry, HealthCheckRating, NewEntry, Patient } from '../../types';
+import { getDiagnoses, addEntry } from '../../services/diagnoses';
 import HospitalEntry from '../EntryComponents/HospitalEntry';
 import OccupationalHealthCareEntry from '../EntryComponents/OccupationalHealthCareEntry';
 import HealthCheckEntry from '../EntryComponents/HealthCheckEntry';
@@ -14,6 +14,12 @@ type Params = {
 const PatientInfoPage = () => {
     const [patient, setPatient] = useState<Patient[]>([]);
     const [diagnoses, setDiagnoses] = useState<Diagnose[]>([]);
+    const [showForm, setShowForm] = useState(false);
+    const [description, setDescription] = useState('');
+    const [date, setDate] = useState('');
+    const [specialist, setSpecialist] = useState('');
+    const [healthCheckRating, setHealthCheckRating] = useState(0);
+    const [diagnosesCodes, setDiagnosesCodes] = useState('');
     const { id } = useParams<Params>();
     useEffect(() => {
         const fetchPatient = async () => {
@@ -50,6 +56,18 @@ const PatientInfoPage = () => {
         throw new Error("Was not never: " + shouldBeNever);
     }
 
+    const handleSubmit = () => {
+        const newObj: NewEntry = {
+            description: description,
+            date: date,
+            specialist: specialist,
+            healthCheckRating: healthCheckRating,
+            diagnosisCodes: [...diagnosesCodes],
+            type: "HealthCheck",
+        };
+        addEntry(id!, newObj);
+    };
+
   return (
     <div>
         {patient.map(details => {
@@ -58,6 +76,20 @@ const PatientInfoPage = () => {
                     <h2>{details.name} {details.gender}</h2>
                     <p>ssh: {details.ssn}</p>
                     <p>occupation: {details.occupation}</p>
+                    {showForm === true? <div><form onSubmit={handleSubmit}>
+                        <h3>New HealthCheck entry</h3>
+                        <p>Description</p>
+                        <input type='text' value={description} onChange={(event) => setDescription(event.target.value)} />
+                        <p>Date</p>
+                        <input type='text' value={date} onChange={(event) => setDate(event.target.value)} />
+                        <p>Specialist</p>
+                        <input type='text' value={specialist} onChange={(event) => setSpecialist(event.target.value)} />
+                        <p>Healthcheck rating</p>
+                        <input type='text' value={healthCheckRating} onChange={(event) => setHealthCheckRating(event.target.value)} />
+                        <p>Diagnosis codes</p>
+                        <input type='text' value={diagnosesCodes} onChange={(event) => setDiagnosesCodes(event.target.value)} />
+                        <button>Add</button>
+                    </form> <button onClick={() => setShowForm(false)}>Close</button></div> : <button onClick={() => setShowForm(true)}>Add new entry</button>}
                     <h3>entries</h3>
                     {details.entries.map(entry => {
                         return (
--- osa9/Patientor-harjoitus/patientor/src/services/diagnoses.ts
+++ osa9/Patientor-harjoitus/patientor/src/services/diagnoses.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
-import { Diagnose } from "../types";
+import { Diagnose, Entry, NewEntry } from "../types";
 
-const getDiagnoses = async () => {
+export const getDiagnoses = async () => {
     const { data } = await axios.get<Diagnose[]>('http://localhost:3001/api/diagnoses');
     return data;
 };
 
-export default getDiagnoses;
+export const addEntry = async (id: string, object: NewEntry) => {
+    const { data } = await axios.post<Entry[]>(`http://localhost:3001/api/patients/${id}/entries`, object);
+    return data;
+};
+
+export default { getDiagnoses, addEntry };
