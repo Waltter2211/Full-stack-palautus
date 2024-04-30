6ee5e5b965299a5396b74d49ce60f31cee82d18d
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-04-30 23:57:29 +0300

    Exercise 12.7: Little bit of MongoDB coding

--- osa12/todo-app/todo-backend/routes/todos.js
+++ osa12/todo-app/todo-backend/routes/todos.js
@@ -29,18 +29,19 @@ const findByIdMiddleware = async (req, res, next) => {
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+  await req.todo.delete()
   res.sendStatus(200);
 });
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo); // Implement this
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  await Todo.findByIdAndUpdate(req.todo, req.body)
+  res.send(req.todo); // Implement this
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
