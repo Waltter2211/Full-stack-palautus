b8f05815383f0b236b6753980fc4ad0179dc5304
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-05-16 19:38:06 +0300

    Tehtävä 13.19. postgresql

--- osa13/postgresql-palautus-blogs/models/index.js
+++ osa13/postgresql-palautus-blogs/models/index.js
@@ -1,12 +1,16 @@
 const User = require('./user')
 const Blog = require('./blog')
+const UserBlogs = require('./userBlogs')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
+
+User.belongsToMany(Blog, { through: UserBlogs })
+Blog.belongsToMany(User, { through: UserBlogs })
 
 /* User.sync({ alter: true })
 Blog.sync({ alter: true }) */
 
 module.exports = {
-  Blog, User
+  Blog, User, UserBlogs
 }
